9cde973eea3955226324ce91e62825d1136817bf
Lanqi2345 <1023457706@qq.com>
2026-08-15 23:26:53 +0800

    sysinfo

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,23 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+
+uint64 freemem(void)
+{
+  struct run *r;
+  uint64 bytes=0;
+
+  acquire(&kmem.lock);
+
+  r=kmem.freelist;
+  while(r!=0)
+  {
+    bytes+=PGSIZE;
+    r=r->next;
+  }
+
+
+  release(&kmem.lock);
+  return bytes;
+}
