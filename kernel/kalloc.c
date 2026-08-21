55513635846d6111120b38aa630544567b4b0338
Lanqi2345 <1023457706@qq.com>
2026-08-21 16:45:06 +0800

    memory allocator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,17 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i=0;i<NCPU;i++)
+  {
+    initlock(&kmem[i].lock, "kmem");
+    kmem[i].freelist = 0;
+  }
+  
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -56,10 +61,16 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  //释放页时，页会进入当前CPU的空闲链表
+  push_off();
+  int id = cpuid();
+
+  acquire(&kmem[id].lock);
+  r->next = kmem[id].freelist;
+  kmem[id].freelist = r;
+  release(&kmem[id].lock);
+
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,11 +81,38 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  push_off();
+  int id = cpuid();
+
+  acquire(&kmem[id].lock);
+  r = kmem[id].freelist;
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem[id].freelist = r->next;
+  
+  release(&kmem[id].lock);
+
+  //当前 CPU 没有空闲页时，从其他 CPU 偷一个
+  if(r == 0)
+  {
+    for(int i = 0; i < NCPU; i++)
+    {
+      if (i == id)
+        continue;
+
+      acquire(&kmem[i].lock);
+
+      r = kmem[i].freelist;
+      if (r)
+        kmem[i].freelist = r->next;
+
+      release(&kmem[i].lock);
+
+      if (r)
+        break;
+    }
+  }
+
+  pop_off();
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
